75a652487d1fe94a533046ccc2215b7bfa5c93a1
harupy <hkawamura0130@gmail.com>
2019-12-04 12:28:27 +0900

    udpate

--- .github/actions/create-jira-link/entrypoint.sh
+++ .github/actions/create-jira-link/entrypoint.sh
@@ -4,7 +4,7 @@ import os
 from github import Github
 
 # or using an access token
-g = Github(os.getenv('TOKEN'))
+g = Github(os.getenv('GITHUB_TOKEN'))
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
