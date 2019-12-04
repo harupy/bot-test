6b1a0b081a2c1ba570e085f2986f4a6cef981922
harupy <hkawamura0130@gmail.com>
2019-12-04 13:03:41 +0900

    update

--- .github/actions/create-jira-link/main.py
+++ .github/actions/create-jira-link/main.py
@@ -2,9 +2,11 @@ import os
 import json
 from github import Github
 
+
 def read_json(filepath):
     with open(filepath, 'r') as f:
         return json.load(f)
+
 
 def main():
     # or using an access token
@@ -16,7 +18,7 @@ def main():
     repo = g.get_repo(event['repository']['full_name'])
     pulls = repo.get_pulls(state='open', sort='created', base='master')
     for pr in pulls:
-        print(pr.comments)
+        print(pr.get_comments())
 
     # Then play with your Github objects:
     for repo in g.get_user().get_repos():
