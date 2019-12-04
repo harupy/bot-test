07d9fd2b8e1a442c51fba0c3f0aa219b2fba14e7
harupy <hkawamura0130@gmail.com>
2019-12-04 12:31:39 +0900

    use python script

---
+++ .github/actions/create-jira-link/main.py
@@ -0,0 +1,9 @@
+import os
+from github import Github
+
+# or using an access token
+g = Github(os.getenv('GITHUB_TOKEN'))
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+    print(repo.name)
