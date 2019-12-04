0080354d85225d59c4cf2df2f0a60c0e0fddc1d4
harupy <hkawamura0130@gmail.com>
2019-12-04 13:08:51 +0900

    update

--- .github/actions/create-jira-link/main.py
+++ .github/actions/create-jira-link/main.py
@@ -17,6 +17,7 @@ def main():
 
     repo = g.get_repo(event['repository']['full_name'])
     pulls = repo.get_pulls(state='open', sort='created', base='master')
+    print('pulls:', len(pulls))
     for pr in pulls:
         for comment in pr.get_comments():
             print(comment)
